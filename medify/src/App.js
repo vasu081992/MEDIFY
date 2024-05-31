d463a4e4110f4fbad11187268bc35d36634ff145
vasu081992 <vasudevan29.92@gmail.com>
2024-05-31 22:55:12 +0530

    fetching cities and states

--- medify/src/App.js
+++ medify/src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import NavBar from './components/navbar/navbar';
 import HeroSlider from './components/HeroSection/Hero';
+import Search from './components/Search/Search';
+
 
 function App() {
   return (
     <div className="App">
      <NavBar/>
      <HeroSlider/>
+     <Search/>
     </div>
   );
 }
