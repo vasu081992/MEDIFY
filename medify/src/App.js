122aa7340db9bc251ac71776450075cba2c67707
vasu081992 <vasudevan29.92@gmail.com>
2024-05-31 21:58:11 +0530

    hero section

--- medify/src/App.js
+++ medify/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import NavBar from './components/navbar/navbar';
+import HeroSlider from './components/HeroSection/Hero';
 
 function App() {
   return (
     <div className="App">
      <NavBar/>
+     <HeroSlider/>
     </div>
   );
 }
